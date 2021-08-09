f1736298ef0a12d30253d2aa5b5b4ef4a8666518
Manoj Kelath <manojkelath1987@gmail.com>
2021-08-09 15:34:01 -0400

    fix receive module component

--- src/app/modules/pages/receive/receive.module.ts
+++ src/app/modules/pages/receive/receive.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReceiveComponent } from './receive.component';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from '../dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent
+    component: ReceiveComponent
   }
 ];
 
