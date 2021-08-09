febda196f0e295f02d04c0c030a23c6cdbac0f41
Manoj Kelath <manojkelath1987@gmail.com>
2021-08-09 15:58:22 -0400

    add shared module to receive module for importing page container

--- src/app/modules/pages/receive/receive.module.ts
+++ src/app/modules/pages/receive/receive.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReceiveComponent } from './receive.component';
@@ -16,6 +17,7 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ]
 })
